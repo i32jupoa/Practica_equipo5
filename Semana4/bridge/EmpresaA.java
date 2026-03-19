38339711a67d749ba5f46c2ac075cdf6ffabce33
Alvaro Jurado <i32jupoa@uco.es>
2026-03-19 13:14:04 +0100

    Creación EmpresaA.java, Catalogo.java y Main.java

--- Semana4/bridge/EmpresaA.java
+++ Semana4/bridge/EmpresaA.java
@@ -1,2 +1,13 @@
-package Semana4.bridge;
+package bridge;
+import java.util.Arrays;
+import java.util.List;
 
+public class EmpresaA implements Proveedor {
+    @Override
+    public List<Producto> obtenerProductos() {
+        return Arrays.asList(
+            new Sofa("Sofá Chaise Longue", 450.0, 5, 3),
+            new Sofa("Sofá Piel Clásico", 600.0, 0, 2) // Sin stock
+        );
+    }
+}
