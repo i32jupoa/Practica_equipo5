e02d9d803c338e63bf5dd78ea33c0fda3a2e991b
adamgalvez <i32garea@uco.es>
2026-05-08 12:01:51 +0200

    Modifciacion FactoriaRestaurante.java y Main.java

--- Practica2/Semana1/abstractfactory/FactoriaRestaurante.java
+++ Practica2/Semana1/abstractfactory/FactoriaRestaurante.java
@@ -2,21 +2,24 @@ package abstractfactory;
 
 public class FactoriaRestaurante implements FactoriaAbstracta {
 
+    // REFACTORIZACIÓN (Semana 2 - Comentarios y Formato): Se eliminan comentarios redundantes que explicaban la intención del código. Además, se añade espaciado vertical para agrupar lógicamente la instanciación y la asignación de platos (Formato Vertical).
     @Override
     public Menu crearMenuSemanal(Acompanamiento acompanamiento) {
         Menu menu = new MenuSemanal();
-        // El menú en el restaurante incluye entrante, principal y postre 
+        
         menu.asignarPlato(new Plato("Sopa de picadillo", 5.0f, TipoPlato.ENTRANTE, Acompanamiento.NINGUNO));
-        // Se puede elegir el acompañamiento en el menú 
         menu.asignarPlato(new Plato("Solomillo al punto", 12.0f, TipoPlato.PRINCIPAL, acompanamiento));
         menu.asignarPlato(new Plato("Tarta de queso", 4.5f, TipoPlato.POSTRE, Acompanamiento.NINGUNO));
+        
         return menu;
     }
 
     @Override
     public Menu crearMenuTemporada() {
         Menu menu = new MenuTemporada();
+        
         menu.asignarPlato(new Plato("Guiso de la abuela", 18.0f, TipoPlato.UNICO, Acompanamiento.NINGUNO));
+        
         return menu;
     }
 }
