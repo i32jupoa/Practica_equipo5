f45b21a0ec3fe4bcff43579a26793ea1b748403a
adamgalvez <i32garea@uco.es>
2026-05-13 13:18:05 +0200

    Refactorización Facade/Main.java

--- Practica2/Semana5/facade/Main.java
+++ Practica2/Semana5/facade/Main.java
@@ -2,25 +2,24 @@ package facade;
 
 public class Main {
     public static void main(String[] args) {
-        // Instanciamos la fachada (el cliente solo conoce esta clase)
         BuscadorViajesFacade agenciaDeViajes = new BuscadorViajesFacade();
 
+        // REFACTORIZACIÓN AUTOMÁTICA (Semana 5): Se utiliza la herramienta "Extract Variable" del IDE 
+        // para extraer las fechas literales a variables descriptivas, mejorando la legibilidad de la llamada.
+        
         // Caso de Uso 1: Viaje a Madrid
-        agenciaDeViajes.buscarViajeCompleto(
-            "15/10/2026", "20/10/2026",
-            "Sevilla", "Madrid"
-        );
+        String fechaIdaMadrid = "15/10/2026";
+        String fechaVueltaMadrid = "20/10/2026";
+        agenciaDeViajes.buscarViajeCompleto(fechaIdaMadrid, fechaVueltaMadrid, "Sevilla", "Madrid");
 
         // Caso de Uso 2: Viaje a Córdoba
-        agenciaDeViajes.buscarViajeCompleto(
-            "01/05/2026", "05/05/2026",
-            "Sevilla", "Córdoba"
-        );
+        String fechaIdaCordoba = "01/05/2026";
+        String fechaVueltaCordoba = "05/05/2026";
+        agenciaDeViajes.buscarViajeCompleto(fechaIdaCordoba, fechaVueltaCordoba, "Sevilla", "Córdoba");
 
-        // Caso de Uso 3: Viaje a una ciudad sin transporte ni turismo (simulando falta de stock)
-        agenciaDeViajes.buscarViajeCompleto(
-            "10/11/2026", "15/11/2026",
-            "Córdoba", "Valencia"
-        );
+        // Caso de Uso 3: Viaje a Valencia
+        String fechaIdaValencia = "10/11/2026";
+        String fechaVueltaValencia = "15/11/2026";
+        agenciaDeViajes.buscarViajeCompleto(fechaIdaValencia, fechaVueltaValencia, "Córdoba", "Valencia");
     }
 }
